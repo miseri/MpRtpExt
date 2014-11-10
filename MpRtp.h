dc106dcae8f7fa258c948bd145d2814f25cf61ba
miseri <ralf.globisch@gmail.com>
2014-11-10 21:15:40 +0200

    Updated comments

--- MpRtp.h
+++ MpRtp.h
@@ -11,7 +11,9 @@
  */
 typedef struct FlowContext
 {
+  // flow-specific sequence number
   int FSSN;
+  // flow id
   int FlowId;
 }FlowContext;
 /**
@@ -22,7 +24,9 @@ typedef struct MPRTPHeaderContext
 {
   // 0 = round-robin
   int SchedulerType;
+  // state of flows
   FlowContext Flows[2];
+  // used to keep track of where last packet was sent
   int LastFlow;
 }MPRTPHeaderContext;
 
@@ -31,19 +35,20 @@ extern "C" {
 #endif
 
 /**
- * @brief initialises the MPRTP context (random SNs for the two flows
+ * @brief initialises the MPRTP context (random SNs for the two flows)
  * @param[out] pContext MPRTP context
  */
 extern void initContext(MPRTPHeaderContext* pContext);
 
 /**
  * @brief inserts an MPRTP header into the RTP packet. Scheduling occurs based on the passed in context.
- * Currently only round-robin is supported by the context.
+ * Currently only round-robin is supported by the context. The caller is responsible for memory allocation.
+ *
  * @param[in] pContext MPRTP context
  * @Param[in] pRtpPacket pointer to RTP packet
  * @Param[in] uiRtpSize size of RTP packet
- * @Param[out] pMpRtpPacket pointer to RTP packet
- * @Param[in, out] uiMpRtpSize size of RTP packet
+ * @Param[out] pMpRtpPacket pointer to MPRTP packet
+ * @Param[in, out] uiMpRtpSize size of MPRTP packet
  * @return returns 0 if successful,
  */
 extern int insertMPRTPHeader(MPRTPHeaderContext* pContext, const unsigned char* pRtpPacket, unsigned uiRtpSize, unsigned char* pMpRtpPacket, unsigned uiMpRtpSize);
